8da938384e950ce8f262f7b73af26ac914c2e589
Bioastroiner <naderi81eco@gmail.com>
2021-04-15 01:40:03 +0430

    potentially fixed the color problem

--- src/main/java/com/bioast/gttools/client/ClientSetup.java
+++ src/main/java/com/bioast/gttools/client/ClientSetup.java
@@ -5,7 +5,6 @@ import com.bioast.gttools.common.item.LumberAxe;
 import com.bioast.gttools.common.item.ModItemTier;
 import com.bioast.gttools.common.item.Saw;
 import com.bioast.gttools.core.setup.ModItems;
-import net.minecraft.item.ItemStack;
 import net.minecraftforge.client.event.ColorHandlerEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
@@ -24,15 +23,16 @@ public class ClientSetup {
             event.getItemColors().register(Saw::getItemColor, item.get());
         });
         ModItems.CraftingMats.forEach((i,m)->m.forEach((tier,item)->{
-            event.getItemColors().register((s,l)->colorWrapper(s,l,0,((ModItemTier)tier).getColor()));
+            //event.getItemColors().register((s,l)->colorWrapper(s,l,0,((ModItemTier)tier).getColor()));
+            event.getItemColors().register((stack,layer)->((ModItemTier)tier).getColor());
         }));
     }
 
-    private static int colorWrapper(ItemStack stack,int layer, int layerToColor,int color){
-//        if (layer == layerToColor) {
-//            return color;
-//        }
-//        return 0xFFFFFF;
-        return color;
-    }
+//    private static int colorWrapper(ItemStack stack,int layer, int layerToColor,int color){
+////        if (layer == layerToColor) {
+////            return color;
+////        }
+////        return 0xFFFFFF;
+//        return color;
+//    }
 }
